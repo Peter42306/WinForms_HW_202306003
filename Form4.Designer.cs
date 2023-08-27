a9e00c4b7184948d2cac82e452ba811ea0300bf8
Peter42306 <pzalizko@gmail.com>
2023-08-27 17:59:29 +0300

    HW 2023 06 03, added task 2

---
+++ Form4.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WinFormsFirstLessonWithButtons
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(82, 52);
+            button1.Name = "button1";
+            button1.Size = new Size(122, 65);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form4
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 456);
+            Controls.Add(button1);
+            Name = "Form4";
+            Text = "HW 2023 06 03, Task 2, Button Run After Click";
+            Load += Form4_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
---
+++ Form4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsFirstLessonWithButtons
+{
+    public partial class Form4 : Form
+    {
+        private Random random = new Random(); // Создаем экземпляр генератора случайных чисел
+
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            MoveButtonToRandomPosition(); // При загрузке формы перемещаем кнопку в случайное положение
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SoundWhenClick(); // издаём бип при нажатии
+            MoveButtonToRandomPosition(); // При клике на кнопку перемещаем ее в случайное положение
+        }
+        private void MoveButtonToRandomPosition()
+        {
+            int maxPositionX = ClientSize.Width - button1.Width; // Максимальная позиция по X, учитывая длину кнопки
+            int maxPositionY = ClientSize.Height - button1.Height; // Максимальная позиция по Y, учитывая высоту кнопки
+
+            int newPositionX = random.Next(maxPositionX); // Генерируем случайное значение X
+            int newPositionY = random.Next(maxPositionY); // Генерируем случайное значение Y
+
+            button1.Location = new Point(newPositionX, newPositionY); // Устанавливаем новое положение кнопки
+        }
+        private void SoundWhenClick()
+        {
+            Console.Beep(); // издаём бип
+        }
+    }
+}
